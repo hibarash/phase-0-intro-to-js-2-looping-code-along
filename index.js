3663540ba82d53db049bf632bc9ede959c1c9c8b
hiba <hibzrashed@gmail.com>
2022-05-23 11:13:22 -0500

    commit tree 040c426fcdd76fa8bd4ee1c6b825d79b4759a1de

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+       
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+*/
+const newArr = [];
+
+function writeCards(name, event) {
+    let newArr = [];
+    for(let i = 0; i < name.length; i++){
+        
+        newArr.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+        
+    }
+    return newArr
+}
+writeCards = (["Sarah", "Fai", "Tom"], surprise);
+
+function countDown(init = 10) {
+    while (init >= 0){
+        console.log(init--);
+    }
+}
